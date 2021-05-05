5c1d40dc69896bf1001ff9f3331064d242cf96e1
LoaiAkram <lolyakram11@gmail.com>
2021-05-05 22:22:26 +0200

    fixed spam

--- commands/spam.py
+++ commands/spam.py
@@ -1,10 +1,12 @@
 from discord.ext import commands
+from permissions import is_developer
 
 
 def setup(bot):
     bot.add_command(spam)
 
 
+@is_developer()
 @commands.command()
 async def spam(ctx, number=100, *, message="spam"):
     for x in range(number):
